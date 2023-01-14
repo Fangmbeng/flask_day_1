8e120b5730062c6a26c6600454015a2797cfdd2d
Fangmbeng <yorobeau@gmail.com>
2023-01-14 00:06:42 -0700

    Fangmbneg

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,9 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    colors = ['success', 'primary', 'secondary', 'warning', 'danger']
-    return render_template('base.html', name = 'Brandon', colors = colors)
+    return render_template('base.html')
 
-@app.route("/posts")
-def posts():
-    return "Brandon Posts"
+@app.route("/favourites")
+def favourites():
+    colors = ['success', 'primary', 'secondary', 'warning', 'danger']
+    return render_template('favourites.html', name = 'Brandon', colors = colors)
